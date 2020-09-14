f3d28005278fe20e3f5343d9756cee5e9a375343
bond-huang <58139438+bond-huang@users.noreply.github.com>
2020-09-14 22:12:20 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,12 @@ if [ $? -ne 0 ];then
     exit 1
 fi
 cd _book
-sed -i '/a href.*\.md/s#\.md#.html#g;/a href.*README\.html/s#README\.html##g' SUMMARY.html
+sed -i '/a href.*\.md/s#\.md#.html#g;/a href.*README\.html/s#README\.html##g' Summary.html
 git init
 git checkout --orphan gh-pages
 git status
 sleep 5
 git add .
 git commit -m "Update gh-pages"
-git remote add origin git@github.com:bond-huang/huang.git
+git remote add origin git@github.com:bond-huang/ebook.git
 git push -f "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages
