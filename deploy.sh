4a9d6dfeee315ba9ab196150c0227c2d6b766715
bond-huang <58139438+bond-huang@users.noreply.github.com>
2020-09-28 00:30:00 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,6 @@ git config --global core.quotepath false
 git checkout -b gitbook
 git status
 git add .
-git commit -a
-git status
-git add .
 git commit -m "[Travis] Update SUMMARY.md"
 git push -f "https://${GH_TOKEN}@${GH_REF}" gitbook:gitbook
 gitbook install
